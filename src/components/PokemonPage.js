95a91666763863d1abd5a1b7c97c5cba299e92fb
Greg Schmitt <gregjschmitt@gmail.com>
2022-03-22 20:35:37 -0400

    done

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/pokemon`)
+      .then((resp) => resp.json())
+      .then((items) => setPokemon(items));
+  }, [])
+
+  function handleNewPokemon(newPokemon) {
+    setPokemon([...pokemon, newPokemon])
+  }
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onNewPokemon={handleNewPokemon} />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection
+        pokemon={searchTerm === "" ? pokemon : pokemon.filter((mon) => 
+          mon.name.toUpperCase().includes(searchTerm.toUpperCase()))}
+        
+      />
     </Container>
   );
 }
